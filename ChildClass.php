26755f10f09774213bbddaa9bbc21491eaba0ac5
Alex Bush <alexandrabush1@gmail.com>
2017-03-26 17:03:10 -0400

    filled in classes and formatted page

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,63 @@
 <?php 
-	// this file will extend PArentClass.php
+	// this file will extend Player.php
 
-	class ChildClass extends ParentClass {
-		
+	class ChildClass extends Player{
+		public $name; //username
+	    public $xp; //total experience
+	    public $st; //skill total
+	    public $ttp; //total time played in hours
+	    public $aoa; //age of the account in years
+	    public $tw; //total wealth
+	    public $wcxp; //woodcutting xp
+	    public function __construct($n, $x, $s, $t, $a, $w, $wcxp) {
+	        $this->username   = $n;
+	        $this->xp         = $x;
+	        $this->skill      = $s;
+	        $this->time       = $t;
+	        $this->age        = $a;
+	        $this->wealth     = $w;
+	       	$this->woodcuttingxp = $wcxp;
+   		}
+   		public function getWoodcuttingXP(){
+   		  return $this->woodcuttingxp;
+   		}
+   		public function addWoodcuttingXP($newWoodXP){
+   			$this->woodcuttingxp += $newWoodXP;
+   			$this->addTotalXP($newWoodXP);
+   			$this->addSkillTotal(94);
+   			$this->addWealth(15000000);
+   			$this->addTimePlayed(72);
+   		}
+
+	    public function getOldTotalXP() {
+	      return parent::getTotalXP();
+	    }
+	    private function addTotalXP($newXP) {
+	      echo "<!-- Adding xp to old total xp " . $newXP . "-->";
+	      $this->xp = ($newXP + $this->getOldTotalXP());
+	    }
+	    public function getOldSkillTotal() {
+	      return parent::getSkillTotal();
+	    }
+	    private function addSkillTotal($newLevels) {
+	      echo "<!-- Setting new skill total to " . $newLevels . "-->";
+	      $this->skill= ($newLevels + $this->getOldSkillTotal());
+	    }
+	    public function getOldTimePlayed() {
+	      return parent::getTimePlayed();
+	    }
+	    private function addTimePlayed($newHours) {
+	      echo "<!-- Setting new time played to " . $newHours . "-->";
+	      $this->time = ($newHours + $this->getOldTimePlayed());
+	    }
+	   	public function getOldTotalWealth() {
+	      return parent::getTotalWealth();
+	    }
+	    private function addWealth($newWealth) {
+	      echo "<!-- Setting new time played to " . $newWealth . "-->";
+	      $this->wealth = ($newWealth + $this->getOldTotalWealth());
+	    }
+	    public function __toString() {
+     		return $parent = parent::__toString();
+   		}
 	}
--- assignment3.php
+++ assignment3.php
@@ -1,18 +1,48 @@
-<?php 
-
-	
-
+<?php
+include 'Player.php';
+include 'ChildClass.php';
+$expert = new Player("Neonn", 181259993, 2318, 2568, 9.5, 530432771);
+//$noob = new Player("Xx-pkdURmom-xX", 13124556, 957, 107, 3, 4000451);
+$noob = new ChildClass("Xx-pkdURmom-xX", 13124556, 957, 107, 3, 4000451, 0);
 ?>
 
 <!doctype html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Assignment 3</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="assignment3.css">
+        <title>
+            How noob are you?
+        </title>
+    </head>
+    <body id="info">
+        <h1>
+            Runescape Player Account Examples
+        </h1>
+        <section>
+            <h4>
+                This is an example of an expert account.
+            </h4>
+            <img src="images/boss.png">
+            <p><? echo $expert ?></p>
+            <h4>
+                This is an example of a noob account.
+            </h4>
+            <img src="images/noob.jpg">
+            <p><? echo $noob ?></p>
+            <h4> However, after being pwn'd by Neonn, pkdURmom wanted to git gud. </h4> 
+            <p> pkdURmom, having <? echo $noob->getWoodcuttingXP() ?> in the Woodcutting skill, decided to not stop chopping virtual wood until her account was awesome.</p>
+            <p><?= $noob->addWoodcuttingXP(9280113);?></p>
+            <img src="images/Woodcutting_Tree.gif">
+            <p> After gaining a massive <? echo $noob->getWoodcuttingXP()?> experience points, pdkURmom had some new account stats to show off.</p>
+            <h4>
+               This is an example of a mid-level account.
+            </h4> 
+            <p><? echo $noob ?></p>
 
-	<link rel="stylesheet" href="assignment3.css">
-</head>
-<body>
-	
-</body>
+        </section>
+    </body>
 </html>
